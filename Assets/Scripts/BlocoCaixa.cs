aca20db1a6ee6c8b0983063e83778d18ccddad51
Filipe de Oliveira de Ataíde <filipe.ataide@lais.huol.ufrn.br>
2022-05-27 09:10:59 -0300

    Arrastar e soltar funcionando.

--- Assets/Scripts/BlocoCaixa.cs
+++ Assets/Scripts/BlocoCaixa.cs
@@ -1,5 +1,19 @@
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
 
-public class BlocoCaixa : MonoBehaviour {
+public class BlocoCaixa : MonoBehaviour, IDragHandler, IEndDragHandler {
     public Bloco bloco;
+    public bool dragging, used;
+
+    public void OnDrag(PointerEventData data){
+        dragging = true;
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData data){
+        dragging = false;
+    }
 }
--- Assets/Scripts/BlocoCampo.cs
+++ Assets/Scripts/BlocoCampo.cs
@@ -1,6 +1,48 @@
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using TMPro;
 
-public class BlocoCampo : MonoBehaviour {
+public class BlocoCampo : MonoBehaviour
+{
     public string resposta;
     public Bloco bloco;
+
+    bool isFull;
+
+    public Button getBotao()
+    {
+        GameObject[] botoes = GameObject.FindGameObjectsWithTag("Botao");
+        foreach (var b in botoes)
+        {
+            Botao botao = b.GetComponent<Botao>();
+            if (botao.bloco == bloco)
+            {
+                return b.GetComponent<Button>();
+            }
+        }
+        return null;
+    }
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+
+        BlocoCaixa caixa = col.gameObject.GetComponent<BlocoCaixa>();
+
+        if (!caixa.dragging && !caixa.used)
+        {
+            if (isFull)
+            {
+                getBotao().interactable = true;
+            }
+
+            bloco = caixa.bloco;
+            transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = bloco.texto;
+            GetComponent<Image>().color = col.gameObject.GetComponent<Image>().color;
+            getBotao().interactable = false;
+            caixa.used = true;
+            isFull = true;
+            Destroy(col.gameObject);
+        }
+    }
 }
